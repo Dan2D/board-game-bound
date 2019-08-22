cf13106ac9ee1e0aa1b5e7001694a59ab7f4d957
Dan Schultz <travelguy90@gmail.com>
2019-08-22 14:23:50 -0700

    Updates

---
+++ precache-manifest.7532f836a5b341a6f88ae3df3a5d242e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1be4d64c6a22045e9093a2f6fae4c37a",
+    "url": "/board-game-bound/index.html"
+  },
+  {
+    "revision": "172712abf3b9dec601ff",
+    "url": "/board-game-bound/static/css/2.3c1ddb0a.chunk.css"
+  },
+  {
+    "revision": "172712abf3b9dec601ff",
+    "url": "/board-game-bound/static/js/2.0959f710.chunk.js"
+  },
+  {
+    "revision": "0c53d3676647da3fe60f",
+    "url": "/board-game-bound/static/js/main.90bfffe9.chunk.js"
+  },
+  {
+    "revision": "f2203cb1edaf0c526164",
+    "url": "/board-game-bound/static/js/runtime~main.7b7a5e39.js"
+  },
+  {
+    "revision": "1e49d7a15f1f18719748d6fa2778364a",
+    "url": "/board-game-bound/static/media/bg-overlay.1e49d7a1.png"
+  }
+]);
